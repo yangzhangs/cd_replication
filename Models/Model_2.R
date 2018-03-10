7ae3efddb72e1625c498eea5e59adfe00bc965f0
wwwyw <33672952+Anonymous-cd@users.noreply.github.com>
2018-03-10 11:40:45 +0800

    Update Model_2.R

--- Models/Model_2.R
+++ Models/Model_2.R
@@ -11,6 +11,14 @@ Table<-read.csv('model_2.csv')
 outliers<-quantile(Table$nErrorBuild,0.99)
 Table_final<-subset(Table,Table$nErrorBuild<outliers)
 
+#We used effect coding to set the contrasts of this three-way factor, 
+#i.e., comparing each level to the grand mean of all three levels.
+#In practice, we change the order of the three levels and run 
+#the model twice to get the coefficients of all three workflows.
+Table_final$workflow<-factor(Table_final$workflow,levels = c("Circle","Travis","Docker"))
+#Table_final$workflow<-factor(Table_final$workflow,levels = c("Circle","Docker","Travis"))
+
+
 #run modeling
 model_2<- lmer(scale(log(nErrorBuild))~ 
                   scale(totalCommits)
@@ -26,3 +34,9 @@ model_2<- lmer(scale(log(nErrorBuild))~
 summary(model_2)
 anova(model_2)
 r.squaredGLMM(model_2)
+
+#We employ multiple hypothesis correction over all coefficient results,
+#to correct for false positives, using the Benjamini-Hochberg step-down procedure.
+pvec_all = summary(model_2)$coefficients[,5]
+pvec_corrected = p.adjust(pvec_all, "BH")
+pvec_corrected
