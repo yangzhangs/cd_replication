cbf4a798c2ea9731e9523db09a5fdc721a59b84e
CD <33672952+Anonymous-cd@users.noreply.github.com>
2018-03-26 22:30:33 +0800

    Update multiple_hyp_correction.R

--- Models/multiple_hyp_correction.R
+++ Models/multiple_hyp_correction.R
@@ -1,10 +1,10 @@
 #We employ multiple hypothesis correction over all coefficient results,
 #to correct for false positives, using the Benjamini-Hochberg step-down procedure.
-pvec_modle_1 = summary(model_1)$coefficients[,5]
-pvec_modle_2 = summary(model_2)$coefficients[,5]
-pvec_modle_3 = summary(model_3)$coefficients[,5]
-pvec_modle_4 = summary(model_4)$coefficients[,5]
+pvec_model_1 = summary(model_1)$coefficients[,5]
+pvec_model_2 = summary(model_2)$coefficients[,5]
+pvec_model_3 = summary(model_3)$coefficients[,5]
+pvec_model_4 = summary(model_4)$coefficients[,5]
 
-pvec_all = c(pvec_modle_1,pvec_modle_2,pvec_modle_3,pvec_modle_4)
+pvec_all = c(pvec_model_1,pvec_model_2,pvec_model_3,pvec_model_4)
 pvec_corrected = p.adjust(pvec_all, "BH")
 pvec_corrected
